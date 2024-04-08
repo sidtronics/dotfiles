722bfe0bcd9bd0dc5de3d89c80bf87c55a5cc74a
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-04-08 23:31:55 +0530

    feat: improved scripts and matched icon theme.

--- ratpoison/.config/ratpoison/scripts/audio.sh
+++ ratpoison/.config/ratpoison/scripts/audio.sh
@@ -4,6 +4,7 @@
 # For this script to work you need pamixer, dunst.
 # By Siddhesh Dharme <siddheshdharme18@gmail.com>
 
+icon_path="$HOME/.config/ratpoison/icons/"
 timeout=1500
 
 case $1 in
@@ -11,30 +12,30 @@ case $1 in
     inc)
         pamixer -i 2
         volume=$(pamixer --get-volume)
-        notify-send Volume $volume% -t $timeout -u low -i audio-volume-high-symbolic -h int:value:$volume -h string:x-dunst-stack-tag:volume 
+        notify-send Volume $volume% -t $timeout -u low -i $icon_path/audio-volume-high-symbolic.svg -h int:value:$volume -h string:x-dunst-stack-tag:volume 
     ;;
 
     dec)
         pamixer -d 2
         volume=$(pamixer --get-volume)
-        notify-send Volume $volume% -t $timeout -u low -i audio-volume-high-symbolic -h int:value:$volume -h string:x-dunst-stack-tag:volume
+        notify-send Volume $volume% -t $timeout -u low -i $icon_path/audio-volume-low-symbolic.svg -h int:value:$volume -h string:x-dunst-stack-tag:volume
     ;;
 
     mute)
         pamixer -t
         if [ $(pamixer --get-mute) = "true" ]; then
-            notify-send Volume Mute -t $timeout -u low -i audio-volume-muted-symbolic -h string:x-dunst-stack-tag:volume
+            notify-send Volume Mute -t $timeout -u low -i $icon_path/audio-volume-muted-symbolic.svg -h string:x-dunst-stack-tag:volume
         else
-            notify-send Volume Unmute -t $timeout -u low -i audio-volume-high-symbolic -h string:x-dunst-stack-tag:volume
+            notify-send Volume Unmute -t $timeout -u low -i $icon_path/audio-volume-high-symbolic.svg -h string:x-dunst-stack-tag:volume
         fi
     ;;
 
     mmute)
         pamixer --default-source -t
         if [ $(pamixer --default-source --get-mute) = "true" ]; then
-            notify-send "Mic Status" Mute -t $timeout -u low -i microphone-sensitivity-muted-symbolic -h string:x-dunst-stack-tag:volume
+            notify-send "Mic Status" Mute -t $timeout -u low -i $icon_path/microphone-sensitivity-muted-symbolic.svg -h string:x-dunst-stack-tag:volume
         else
-            notify-send "Mic Status" Unmute -t $timeout -u low -i microphone-sensitivity-high-symbolic -h string:x-dunst-stack-tag:volume
+            notify-send "Mic Status" Unmute -t $timeout -u low -i $icon_path/microphone-sensitivity-high-symbolic.svg -h string:x-dunst-stack-tag:volume
         fi
     ;;
 
--- ratpoison/.config/ratpoison/scripts/brightness.sh
+++ ratpoison/.config/ratpoison/scripts/brightness.sh
@@ -4,20 +4,22 @@
 # For this script to work you need light, dunst.
 # By Siddhesh Dharme <siddheshdharme18@gmail.com>
 
+icon_path="$HOME/.config/ratpoison/icons/"
+
 case $1 in
     
     inc)
         light -A 2
         temp=$(light -G)
         brightness=${temp%???}
-        notify-send Brightness $brightness% -t 1500 -u low -i display-brightness-symbolic -h int:value:$brightness% -h string:x-dunst-stack-tag:brightness
+        notify-send Brightness $brightness% -t 1500 -u low -i $icon_path/display-brightness-symbolic.svg -h int:value:$brightness% -h string:x-dunst-stack-tag:brightness
     ;;
 
     dec)
         light -U 2
         temp=$(light -G)
         brightness=${temp%???}
-        notify-send Brightness $brightness% -t 1500 -u low -i display-brightness-symbolic -h int:value:$brightness% -h string:x-dunst-stack-tag:brightness
+        notify-send Brightness $brightness% -t 1500 -u low -i $icon_path/display-brightness-symbolic.svg -h int:value:$brightness% -h string:x-dunst-stack-tag:brightness
     ;;
 
 esac
