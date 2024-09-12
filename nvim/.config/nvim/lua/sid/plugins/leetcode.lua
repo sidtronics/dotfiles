6ca69a536fc17a012a00433c12121d3ed0e814f8
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-09-12 15:21:25 +0530

    feat: added new plugin for leetcode integration

---
+++ nvim/.config/nvim/lua/sid/plugins/leetcode.lua
@@ -0,0 +1,35 @@
+local leet_arg = "leetcode.nvim"
+
+return {
+
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim", -- required by telescope
+        "MunifTanjim/nui.nvim",
+
+        -- optional
+        "nvim-treesitter/nvim-treesitter",
+        "rcarriga/nvim-notify",
+        "nvim-tree/nvim-web-devicons",
+    },
+
+    lazy = leet_arg ~= vim.fn.argv()[1],
+
+    opts = {
+        arg = leet_arg,
+
+        injector = {
+            ["cpp"] = {
+                before = {
+                    "#include <bits/stdc++.h>",
+                    "using namespace std;"
+                }
+            }
+        }
+    },
+
+    cmd = "Leet",
+}
