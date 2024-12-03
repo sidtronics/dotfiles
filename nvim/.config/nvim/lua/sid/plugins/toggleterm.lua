2a206d011e8750c37b30dd41327109c77d5dfb08
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-03 14:56:43 +0530

    refactor: added some more options
    - start_in_insert and persist_mode
    - removed some unused keybinds from keys

--- nvim/.config/nvim/lua/sid/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/sid/plugins/toggleterm.lua
@@ -3,13 +3,15 @@ return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
 	lazy = true,
-	keys = { [[<C-\>]], "<C-'>", "<C-@>"},
+	keys = [[<C-\>]],
 
 	opts = {
 
 		open_mapping = [[<C-\>]],
 		direction = "float",
 		auto_scroll = true,
+        start_in_insert = true,
+        persist_mode = false,
 		float_opts = {
 
 			border = "rounded",
