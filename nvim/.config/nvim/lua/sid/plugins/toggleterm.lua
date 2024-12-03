ae3b9e2b54d2448f05818ae3c90aedb69bd6928f
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-04 03:05:14 +0530

    feat: vimpc integration using toggleterm custom terminal

--- nvim/.config/nvim/lua/sid/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/sid/plugins/toggleterm.lua
@@ -1,28 +1,33 @@
 return {
 
-	"akinsho/toggleterm.nvim",
-	version = "*",
-	lazy = true,
-	keys = { [[<C-\>]], "<C-g>" },
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    lazy = true,
+    keys = { [[<C-\>]], "<C-g>", "<leader>vp" },
 
-	opts = {
+    opts = {
 
-		open_mapping = [[<C-\>]],
-		direction = "float",
-		auto_scroll = true,
+        open_mapping = [[<C-\>]],
+        direction = "float",
+        auto_scroll = true,
         start_in_insert = true,
         persist_mode = false,
-		float_opts = {
-			border = "rounded",
-		},
-	},
+        float_opts = {
+            border = "rounded",
+        },
+    },
 
-    config = function (_,opts)
-
+    config = function(_, opts)
         require("toggleterm").setup(opts)
         local Terminal = require("toggleterm.terminal").Terminal
-        local lg = Terminal:new({cmd = "lazygit", hidden = true, float_opts = {width = 120, height = 30}})
-        function _Lazygit_toggle() lg:toggle() end
-        vim.keymap.set({"n", "t"}, "<C-g>", _Lazygit_toggle, {noremap = true, silent = true})
+
+        -- custom terminals:
+        local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, count = 100, float_opts = { width = 120, height = 30 } })
+        function _Lazygit_toggle() lazygit:toggle() end
+        vim.keymap.set({ "n", "t" }, "<C-g>", _Lazygit_toggle, { noremap = true, silent = true })
+
+        local vimpc = Terminal:new({ cmd = "vimpc", hidden = false, count = 1 })
+        function _Vimpc_toggle() vimpc:toggle() end
+        vim.keymap.set("n", "<leader>vp", _Vimpc_toggle, { noremap = true, silent = true })
     end
 }
