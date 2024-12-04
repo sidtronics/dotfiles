d10260aa9176f9a3043be57dbf11763b500f591d
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-05 02:41:40 +0530

    feat: blame every line using blame.nvim

---
+++ nvim/.config/nvim/lua/sid/plugins/blame.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "FabijanZulj/blame.nvim",
+        lazy = true,
+        keys = {
+            { "tb", "<cmd>BlameToggle<cr>", { Desc = "Toggle Blame", noremap = true } }
+        },
+        config = true
+    },
+}
