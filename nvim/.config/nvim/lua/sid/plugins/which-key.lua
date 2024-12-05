308f55ab4ab63edc9b1ca1d854b53ff61baaf36a
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-06 02:40:40 +0530

    feat(nvim/whichkey): enabled group/icons for some mappings and made border rounded

--- nvim/.config/nvim/lua/sid/plugins/which-key.lua
+++ nvim/.config/nvim/lua/sid/plugins/which-key.lua
@@ -11,4 +11,22 @@ return {
             desc = "Buffer",
         },
     },
+
+    opts = {
+
+        spec = {
+            { [[<C-\>]],   desc = "Toggle terminal" },
+            { "<leader>t", group = "Telescope" },
+            { "<leader>l", group = "LSP", icon = "" },
+            { "<leader>g", group = "Git" },
+            { "<leader>m", icon = "" },
+            { "<leader>p", icon = ""},
+            { "<leader>z", icon = "󰒲"},
+        },
+
+        win = {
+
+            border = "rounded"
+        }
+    }
 }
