2ee018db732118946f050a9d5b7fb6797b62f74f
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-06 02:22:29 +0530

    refactor(nvim/telescope): changed prefix and added description
    - changed prefix f -> t
    - added short description for each mapping

--- nvim/.config/nvim/lua/sid/plugins/telescope.lua
+++ nvim/.config/nvim/lua/sid/plugins/telescope.lua
@@ -8,9 +8,9 @@ return {
 
     config = function()
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>tf", builtin.find_files, {desc = "Find Files"})
+        vim.keymap.set("n", "<leader>tg", builtin.live_grep, {desc = "Live Grep"})
+        vim.keymap.set("n", "<leader>tb", builtin.buffers, {desc = "Find Buffer"})
+        vim.keymap.set("n", "<leader>th", builtin.help_tags, {desc = "Help Tags"})
     end,
 }
