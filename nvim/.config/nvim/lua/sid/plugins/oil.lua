1b7b2e50f5ce00d3b39771174421c24ab78d8a5c
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-07 23:33:59 +0530

    feat(nvim): new plugin to manage files (oil.nvim)

---
+++ nvim/.config/nvim/lua/sid/plugins/oil.lua
@@ -0,0 +1,7 @@
+return {
+    "stevearc/oil.nvim",
+    lazy = true,
+    keys = { { "-", "<cmd>Oil --float<cr>", desc = "Oil" } },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {}
+}
