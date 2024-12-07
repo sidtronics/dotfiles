79e2ac22fbceb53581a4503f0509208df473518b
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-07 23:33:13 +0530

    feat(nvim): new plugins to improve editor experience
    - Added nvim-autopairs
    - Added nvim-surround

---
+++ nvim/.config/nvim/lua/sid/plugins/editor.lua
@@ -0,0 +1,16 @@
+return {
+
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+    },
+
+
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        config = true
+    }
+}
