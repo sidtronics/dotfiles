f71de101cfde06ea2111fdac80b504ef891cd422
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-08 18:33:11 +0530

    feat(nvim): vastly improved dashboard

--- nvim/.config/nvim/lua/sid/plugins/ui.lua
+++ nvim/.config/nvim/lua/sid/plugins/ui.lua
@@ -29,7 +29,7 @@ return {
         event = 'VimEnter',
         dependencies = { { 'nvim-tree/nvim-web-devicons' } },
         opts = {
-            theme = "hyper",
+            theme = "doom",
             config = {
                 header = {
 
@@ -43,7 +43,61 @@ return {
                     [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
                     [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
                     [[                                                                       ]],
+                    [[                                                                       ]],
+                    [[                                                                       ]],
 
+                },
+
+                center = {
+
+                    {
+                        icon = ' ',
+                        desc = 'New File',
+                        key = 'n',
+                        action = ':ene'
+                    },
+
+                    {
+                        icon = ' ',
+                        desc = 'Recent Files',
+                        key = 'r',
+                        action = ':Telescope oldfiles'
+                    },
+
+                    {
+                        icon = ' ',
+                        desc = 'Find Files',
+                        key = 'f',
+                        action = ':Telescope find_files'
+                    },
+
+                    {
+                        icon = '󰈸 ',
+                        desc = 'LeetCode',
+                        key = 'l',
+                        action = ':Leet'
+                    },
+
+                    {
+                        icon = ' ',
+                        desc = 'Config',
+                        key = 'c',
+                        action = ':cd ~/.config/nvim/lua/sid | Telescope find_files'
+                    },
+
+                    {
+                        icon = '󰈆 ',
+                        desc = 'Quit',
+                        key = 'q',
+                        action = ':qa'
+                    }
+                },
+            },
+
+            {
+                hide = {
+
+                    statusline = false
                 }
             }
         }
