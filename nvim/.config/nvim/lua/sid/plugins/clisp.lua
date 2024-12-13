2d0bad20195ea201e9b1acd8b360f4906a7693c5
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-13 15:11:58 +0530

    feat(nvim): added plugins for clisp dev environment
    + nvlime for SLime-Emacs like lisp integration
    + parsley (dependency for nvlime)
    + nvim-parinfer for auto indentation

---
+++ nvim/.config/nvim/lua/sid/plugins/clisp.lua
@@ -0,0 +1,17 @@
+return {
+
+    "monkoose/nvlime",
+    ft = "lisp",
+    dependencies = {
+
+        {
+            "monkoose/parsley",
+            lazy = true
+        },
+
+        {
+            "gpanders/nvim-parinfer",
+            lazy = true,
+        }
+    },
+}
