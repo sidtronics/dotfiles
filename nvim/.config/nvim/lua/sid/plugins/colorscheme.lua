f45044644fbea7c91e35650c25154e3b217a23d0
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-19 20:39:20 +0530

    refactor(nvim): going back to tokyonight.nvim

--- nvim/.config/nvim/lua/sid/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/sid/plugins/colorscheme.lua
@@ -3,23 +3,17 @@ return {
     {
 
         "folke/tokyonight.nvim",
-		lazy = true,
+		lazy = false,
 		priority = 1000,
 		opts = {
 
             style = "night",
             transparent = true
-		}
-	},
+		},
 
-	{
-        'iruzo/matrix-nvim',
-		lazy = false,
-		priority = 1000,
-        config = function()
-            vim.g.matrix_disable_background = true
-            require('matrix').set()
-            vim.cmd("colorscheme matrix")
+        config = function(_,opts)
+            require("tokyonight").setup(opts)
+            vim.cmd("colorscheme tokyonight")
         end
-	}
+	},
 }
