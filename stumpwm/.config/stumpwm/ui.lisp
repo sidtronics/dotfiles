eb2b6452f6929aee266f2070a4bcfc48ae362480
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-20 23:28:46 +0530

    feat(stump): added more ui related settings

--- stumpwm/.config/stumpwm/ui.lisp
+++ stumpwm/.config/stumpwm/ui.lisp
@@ -1,4 +1,27 @@
+
+; Font
+(load-module "ttf-fonts")
+(set-font (make-instance 'xft:font
+           :family "FiraCode Nerd Font Propo"
+           :subfamily "Bold"
+           :size 11
+           :antialias t))
+
+;; Gravity
 (setf *input-window-gravity* :center)
+(setf *message-window-gravity* :center)
+(setf *message-window-input-gravity* :top)
+
+;; Borders
+(setf *normal-border-width* 2)
+(setf *maxsize-border-width* 2)
+(setf *mode-line-border-width* 2)
+(set-msg-border-width 2)
+
+;; Window format
+(setf *window-format* "%s%n:%4c%m")
+
+;; Show atmost 5 completion candidates
 (setf *input-refine-candidates-fn*
       (lambda (str candidates)
         (let ((matched
