b8bf87142dfdf0ee01b92e8053ac04b860c23489
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-20 23:27:47 +0530

    feat(stump): added more keymaps

--- stumpwm/.config/stumpwm/keymaps.lisp
+++ stumpwm/.config/stumpwm/keymaps.lisp
@@ -1,10 +1,10 @@
 
-;; Change prefix to Super:
+;;; Change prefix to Super:
 (run-shell-command "xmodmap -e 'clear mod4'" t)
 (run-shell-command "xmodmap -e \'keycode 133 = F20\'" t)
 (set-prefix-key (kbd "F20"))
 
-;; Top-level keymaps:
+;;; Top-level keymaps:
 (define-key *top-map* (kbd "XF86MonBrightnessDown")
   "run-shell-command light -U 2")
 
@@ -22,3 +22,29 @@
 
 (define-key *top-map* (kbd "XF86AudioMicMute")
   "run-shell-command pactl set-source-mute @DEFAULT_SOURCE@ toggle")
+
+(define-key *top-map* (kbd "F2")
+  "run-shell-command mpc toggle")
+
+;;; Root-level keymaps:
+
+;; Window:
+(define-key *root-map* (kbd "RET") "pull-hidden-next")
+
+;; Group:
+(define-key *root-map* (kbd "1") "gselect 1")
+(define-key *root-map* (kbd "2") "gselect 2")
+(define-key *root-map* (kbd "3") "gselect 3")
+(define-key *root-map* (kbd "4") "gselect 4")
+(define-key *root-map* (kbd "5") "gselect 5")
+
+;; Applications
+(define-key *root-map* (kbd "t") "exec alacritty")
+(define-key *root-map* (kbd "b") "exec firefox")
+
+;; Misc:
+(define-key *root-map* (kbd "e") "exec")
+(define-key *root-map* (kbd "ESC") "abort")
+(define-key *root-map* (kbd "B") "banish")
+;(define-key *root-map* (kbd "x") "kill")
+;(define-key *root-map* (kbd "X") "remove")
