b61135703ac579d9b1ae7eb4b5090446df8e3c0c
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-20 23:34:08 +0530

    feat(stump): added battery indicator for modeline

--- stumpwm/.config/stumpwm/modeline.lisp
+++ stumpwm/.config/stumpwm/modeline.lisp
@@ -1,7 +1,22 @@
 
-(setf *screen-mode-line-format*
-  (list "[%n] ^>%d"))
-
 (setf *time-modeline-string* "%a %d/%m/%y %H:%M:%S")
 (setf *mode-line-timeout* 5)
-(mode-line)
+
+(defparameter *battery-icons*
+  (make-array 11
+              :initial-contents
+              '(#\󰂎 #\󰁺 #\󰁻 #\󰁼 #\󰁽 #\󰁾 #\󰁿 #\󰂀 #\󰂁 #\󰂂 #\󰁹)))
+
+(defun battery ()
+  (let*
+      ((cap (parse-integer (uiop:read-file-string "/sys/class/power_supply/BAT0/capacity")))
+       (stat (char (uiop:read-file-string "/sys/class/power_supply/BAT0/status") 0))
+       (icon (aref *battery-icons* (floor (/ cap 10)))))
+    (if (char-equal stat #\D)
+        (format nil "~a ~a%" icon cap)
+        (format nil "~a 󱐋 ~a%" icon cap))))
+
+(setf *screen-mode-line-format*
+      (list "[%n] %v ^>" '(:eval (battery)) " | %d"))
+
+(enable-mode-line (current-screen) (current-head) t)
