dd0e71ce370375079f4aa560b1f4511cb3ac77dc
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-28 18:17:00 +0530

    refactor(nvim): changed telescope keymap and fixed desc

--- nvim/.config/nvim/lua/sid/plugins/telescope.lua
+++ nvim/.config/nvim/lua/sid/plugins/telescope.lua
@@ -5,10 +5,10 @@ return {
     cmd = "Telescope",
     keys = {
 
-        { "<leader>tf", "<cmd>Telescope find_files<cr>", { desc = "Find Files" } },
-        { "<leader>tg", "<cmd>Telescope live_grep<cr>",  { desc = "Live Grep" } },
-        { "<leader>tb", "<cmd>Telescope buffers<cr>",    { desc = "Find Buffers" } },
-        { "<leader>th", "<cmd>Telescope help_tags<cr>",  { desc = "Help Tags" } },
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Live Grep" },
+        { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Find Buffers" },
+        { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Help Tags" },
     },
     tag = "0.1.5",
     dependencies = {
