a4395a0af243ad4ea1ba70b2edfaacd360fdceb7
Siddhesh Dharme <siddheshdharme18@gmail.com>
2024-12-28 18:17:59 +0530

    feat(nvim): new keymap for telescope (recent files)
    - keymap to view recent files

--- nvim/.config/nvim/lua/sid/plugins/telescope.lua
+++ nvim/.config/nvim/lua/sid/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
         { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Live Grep" },
         { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Find Buffers" },
         { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Help Tags" },
+        { "<leader>fr", "<cmd>Telescope oldfiles<cr>",  desc = "Recent Files" },
     },
     tag = "0.1.5",
     dependencies = {
