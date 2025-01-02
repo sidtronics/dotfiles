cf2a0d41a4752e3be449b64356807511f055d546
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-01-02 21:26:41 +0530

    fix(nvim): temporary workaround for files opened without -b flag

--- nvim/.config/nvim/lua/sid/plugins/hex.lua
+++ nvim/.config/nvim/lua/sid/plugins/hex.lua
@@ -1,22 +1,22 @@
 return {
 
-    {
-        "RaafatTurki/hex.nvim",
-        cond = vim.bo.binary,
-        opts = {
-            is_file_binary_pre_read = function()
-                if vim.bo.ft ~= "" then
-                    return false
-                end
 
-                if vim.bo.bin then
-                    return true
-                end
-
-                return false
-            end,
-
-            is_file_binary_post_read = function() return false end,
-        },
-    },
+    "RaafatTurki/hex.nvim",
+    --        cond = vim.bo.binary,
+    --        opts = {
+    --            is_file_binary_pre_read = function()
+    --                if vim.bo.ft ~= "" then
+    --                    return false
+    --                end
+    --
+    --                if vim.bo.bin then
+    --                    return true
+    --                end
+    --
+    --                return false
+    --            end,
+    --
+    --            is_file_binary_post_read = function() return false end,
+    --        },
+    config = true
 }
