21ab06181a856900f9fb28bcc323d5164bc1c6a6
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-02-01 12:52:23 +0530

    refactor(nvim): start hex.nvim only in binary mode

--- nvim/.config/nvim/lua/sid/plugins/hex.lua
+++ nvim/.config/nvim/lua/sid/plugins/hex.lua
@@ -2,21 +2,21 @@ return {
 
 
     "RaafatTurki/hex.nvim",
-    --        cond = vim.bo.binary,
-    --        opts = {
-    --            is_file_binary_pre_read = function()
-    --                if vim.bo.ft ~= "" then
-    --                    return false
-    --                end
-    --
-    --                if vim.bo.bin then
-    --                    return true
-    --                end
-    --
-    --                return false
-    --            end,
-    --
-    --            is_file_binary_post_read = function() return false end,
-    --        },
+    cond = vim.bo.binary,
+    opts = {
+        is_file_binary_pre_read = function()
+            if vim.bo.ft ~= "" then
+                return false
+            end
+
+            if vim.bo.bin then
+                return true
+            end
+
+            return false
+        end,
+
+        is_file_binary_post_read = function() return false end,
+    },
     config = true
 }
