86e85d2740c604e97b612ae5a0f01e80c4d51de7
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-02-20 01:43:09 +0530

    feat(nvim): new plugin for coloured parenthesis

--- nvim/.config/nvim/lua/sid/plugins/editor.lua
+++ nvim/.config/nvim/lua/sid/plugins/editor.lua
@@ -1,18 +1,50 @@
 return {
 
     {
-        'windwp/nvim-autopairs',
+        "windwp/nvim-autopairs",
         event = "InsertEnter",
         opts = {
-            disable_filetype = { "lisp" , "markdown" }
+            disable_filetype = { "lisp", "markdown" },
         },
     },
-
 
     {
         "kylechui/nvim-surround",
         version = "*",
         event = "VeryLazy",
-        config = true
+        config = true,
+    },
+
+    {
+        "HiPhish/rainbow-delimiters.nvim",
+        event = "BufRead",
+        config = function ()
+
+            local rainbow_delimiters = require("rainbow-delimiters")
+
+            require("rainbow-delimiters.setup").setup({
+            strategy = {
+                [""] = rainbow_delimiters.strategy["global"],
+                vim = rainbow_delimiters.strategy["local"],
+            },
+            query = {
+                [""] = "rainbow-delimiters",
+                lua = "rainbow-blocks",
+            },
+            priority = {
+                [""] = 110,
+                lua = 210,
+            },
+            highlight = {
+                "RainbowDelimiterRed",
+                "RainbowDelimiterYellow",
+                "RainbowDelimiterBlue",
+                "RainbowDelimiterOrange",
+                "RainbowDelimiterGreen",
+                "RainbowDelimiterViolet",
+                "RainbowDelimiterCyan",
+            },
+        })
+        end,
     }
 }
