b0e1ba35007ecb2e4405431ba627912811b4c85c
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-04-02 00:08:59 +0530

    refactor(nvim): removed deprecated function
    + removed deprecated function vim.fn.sign_define()
    + using vim.diagnostic.config() instead

--- nvim/.config/nvim/lua/sid/lsp/diagnostics.lua
+++ nvim/.config/nvim/lua/sid/lsp/diagnostics.lua
@@ -1,19 +1,15 @@
-local signs = {
-
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
-	{ name = "DiagnosticSignInfo", text = "" },
-}
-
-for _, sign in ipairs(signs) do
-	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
 vim.diagnostic.config({
 
 	virtual_text = false,
 	update_in_insert = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.INFO] = "",
+        }
+    },
 	float = {
 
 		border = "rounded",
