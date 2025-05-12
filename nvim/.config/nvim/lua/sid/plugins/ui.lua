2004008c895af6dfd8c2096db2a328f0b40d549a
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-05-12 19:49:52 +0530

    refactor(nvim): tweaked lualine's sections look
    + formatting changes

--- nvim/.config/nvim/lua/sid/plugins/ui.lua
+++ nvim/.config/nvim/lua/sid/plugins/ui.lua
@@ -1,10 +1,16 @@
 return {
 
     {
-        'nvim-lualine/lualine.nvim',
+        "nvim-lualine/lualine.nvim",
         event = "VeryLazy",
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {
+
+            options = {
+
+                component_separators = { left = "|", right = "|" },
+                section_separators = { left = "", right = "" },
+            },
 
             sections = {
 
@@ -13,22 +19,22 @@ return {
                     {
                         "buffers",
                         symbols = {
-                            alternate_file = ''
-                        }
-                    }
-                }
-            }
+                            alternate_file = "",
+                        },
+                    },
+                },
+            },
         },
 
         init = function()
             vim.o.showmode = false
-        end
+        end,
     },
 
     {
-        'nvimdev/dashboard-nvim',
-        event = 'VimEnter',
-        dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+        "nvimdev/dashboard-nvim",
+        event = "VimEnter",
+        dependencies = { { "nvim-tree/nvim-web-devicons" } },
         opts = {
             theme = "doom",
             config = {
@@ -48,61 +54,58 @@ return {
                     [[                                                                       ]],
                     [[                                                                       ]],
                     [[                                                                       ]],
-
                 },
 
                 center = {
 
                     {
-                        icon = ' ',
-                        desc = 'New File',
-                        key = 'n',
-                        action = ':ene'
+                        icon = " ",
+                        desc = "New File",
+                        key = "n",
+                        action = ":ene",
                     },
 
                     {
-                        icon = ' ',
-                        desc = 'Recent Files',
-                        key = 'r',
-                        action = ':FzfLua oldfiles'
+                        icon = " ",
+                        desc = "Recent Files",
+                        key = "r",
+                        action = ":FzfLua oldfiles",
                     },
 
                     {
-                        icon = ' ',
-                        desc = 'Find Files',
-                        key = 'f',
-                        action = ':FzfLua files'
+                        icon = " ",
+                        desc = "Find Files",
+                        key = "f",
+                        action = ":FzfLua files",
                     },
 
                     {
-                        icon = '󰈸 ',
-                        desc = 'LeetCode',
-                        key = 'l',
-                        action = ':Leet'
+                        icon = "󰈸 ",
+                        desc = "LeetCode",
+                        key = "l",
+                        action = ":Leet",
                     },
 
                     {
-                        icon = ' ',
-                        desc = 'Config',
-                        key = 'c',
-                        action = ':cd ~/.config/nvim/lua/sid | FzfLua files'
+                        icon = " ",
+                        desc = "Config",
+                        key = "c",
+                        action = ":cd ~/.config/nvim/lua/sid | FzfLua files",
                     },
 
                     {
-                        icon = '󰈆 ',
-                        desc = 'Quit',
-                        key = 'q',
-                        action = ':qa'
-                    }
+                        icon = "󰈆 ",
+                        desc = "Quit",
+                        key = "q",
+                        action = ":qa",
+                    },
                 },
             },
 
-
             hide = {
 
-                statusline = false
-            }
-
-        }
-    }
+                statusline = false,
+            },
+        },
+    },
 }
