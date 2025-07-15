4375337b67078e6d1a8fc1e165f6f44976730251
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-07-15 12:42:47 +0530

    feat(nvim): replaced parinfer to paredit

--- nvim/.config/nvim/lua/sid/plugins/clisp.lua
+++ nvim/.config/nvim/lua/sid/plugins/clisp.lua
@@ -11,9 +11,26 @@ return {
         },
 
         {
-            "gpanders/nvim-parinfer",
+            "kovisoft/paredit",
             lazy = true,
-        },
+            config = function()
+                local wk = require("which-key")
+                wk.add({
+                    mode = "n",
+                    icon = "󰅲",
+                    { "<leader><",      desc = "Slurp/Barf" },
+                    { "<leader>>",      desc = "Slurp/Barf" },
+                    { "<leader>J",      desc = "Join" },
+                    { "<leader>O",      desc = "Split" },
+                    { "<leader>S",      desc = "Splice" },
+                    { "<leader>I",      desc = "Raise" },
+                    { "<leader>(",      desc = "Toggle" },
+                    { "<leader><Up>",   desc = "Splice Next" },
+                    { "<leader><Down>", desc = "Splice Prev" },
+                    { "<leader>W",      desc = "Wrap",       mode = { "n", "v" } },
+                })
+            end
+        }
     },
 
     init = function()
