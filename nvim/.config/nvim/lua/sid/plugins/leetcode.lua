eb47aade5e60f8a8da171a339d7479b8037ac0c2
Siddhesh Dharme <siddheshdharme18@gmail.com>
2025-07-15 12:43:06 +0530

    refactor(nvim): removed telescope

--- nvim/.config/nvim/lua/sid/plugins/leetcode.lua
+++ nvim/.config/nvim/lua/sid/plugins/leetcode.lua
@@ -6,7 +6,6 @@ return {
     build = ":TSUpdate html",
     dependencies = {
 
-        "nvim-telescope/telescope.nvim",
         "nvim-lua/plenary.nvim", -- required by telescope
         "MunifTanjim/nui.nvim",
 
